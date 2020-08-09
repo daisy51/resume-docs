2eb99a9be4302ff935ea56982fc44b72a1bc4361
maple <944123439@qq.com>
2020-08-09 18:48:37 +0800

    update pages

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/about/css/page.css","5fc9a2ba8133f10e62f805ef99cdea53"],["/about/css/style.min.css","16a4cc20ac2ecce2dfd8bc11dcf43592"],["/about/css/vue-pdf.css","87a92787efb89f95f9907fa30774f86a"],["/about/index.html","8a0cf32161cef9267bb3647d8168d79a"],["/about/js/common.js","748b78854064e842c09d1b9605209bba"],["/about/js/css.escape.js","a2185e4074b2acf55b190d49300dc626"],["/about/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"],["/about/zh-cn/index.html","a0b85a173555e7d583f88e90d3e6fb2e"]];
+var precacheConfig = [["resume-docs/css/page.css","5fc9a2ba8133f10e62f805ef99cdea53"],["resume-docs/css/style.min.css","16a4cc20ac2ecce2dfd8bc11dcf43592"],["resume-docs/css/vue-pdf.css","87a92787efb89f95f9907fa30774f86a"],["resume-docs/index.html","153754b6f4a2f624995e69a8608090a7"],["resume-docs/js/common.js","748b78854064e842c09d1b9605209bba"],["resume-docs/js/css.escape.js","a2185e4074b2acf55b190d49300dc626"],["resume-docs/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"],["resume-docs/zh-cn/index.html","b71ce3398e553e05cb3b12e3ced10e53"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
